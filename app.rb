2cd4f053eac46cab478348c2590b6a55228c6dc7
nathanandre <nathanandreburns@gmail.com>
2021-10-06 22:31:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Nathan"
+    end 
+
+    get '/hometown' do 
+        "My hometown is Philadelphia"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is Jesus bE a Fence"
+    end 
 end
